0531960966841f6ac0a167ebd3584924eae700eb
CaptKarnage <alan@karna.us>
2024-04-04 17:25:18 -0500

    +additional test cases to test all SafeAdd functions in fractiontest.cpp +addOverage method to SafeParameters Class +added overage rollover counter to SafeParameters Class

--- fractiontest.cpp
+++ fractiontest.cpp
@@ -10,18 +10,29 @@ using namespace std;
 int main() {
 	
 	//Set up all the test variables
-	uint8_t		n8 	= 201;
-	uint8_t		d8 	= 100;
-	uint8_t		o8	= 0;
-	uint16_t	n16	= 60001;
-	uint16_t	d16 = 6000;
-	uint16_t	o16 = 0;
-	uint32_t	n32 = 4000000001;
-	uint32_t	d32 = 400000000;
-	uint32_t	o32 = 0;
-	uint64_t	n64 = 18000000000000000001;
-	uint64_t	d64 = 500000000000000000;
-	uint64_t	o64 = 0;
+	uint8_t		n8 		= 201;
+	uint8_t		d8		= 100;
+	uint8_t		o8		= 0;
+	uint8_t		mn8		= 10;
+	uint8_t		mx8		= 100;
+	
+	uint16_t	n16		= 60001;
+	uint16_t	d16		= 6000;
+	uint16_t	o16		= 0;
+	uint16_t	mn16	= 1000;
+	uint16_t	mx16	= 10000;
+	
+	uint32_t	n32		= 4000000001;
+	uint32_t	d32		= 400000000;
+	uint32_t	o32		= 0;
+	uint32_t	mn32	= 1000000;
+	uint32_t	mx32	= 1000000000;
+	
+	uint64_t	n64 	= 18000000000000000001;
+	uint64_t	d64 	= 500000000000000000;
+	uint64_t	o64 	= 0;
+	uint64_t	mn64	= 1000000000;
+	uint64_t	mx64	= 5000000000000000000;
 	
 	//Test fraction constructors
 	Fraction	f8( n8, d8 );
@@ -36,30 +47,65 @@ int main() {
 	uint32_t	s32 = safeAdd_quick<uint32_t>( n32, d32 );
 	uint64_t	s64 = safeAdd_quick<uint64_t>( n64, d64 );
 
-	cout << s8  << endl; //Outputs as char... because uint8_t doesn't really exist as an unsigned integer type
-	cout << s16 << endl;
-	cout << s32 << endl;
-	cout << s64 << endl;
-	cout << endl;
+	cout	<< s8  	<< endl; //Outputs as char... because uint8_t doesn't really exist as an unsigned integer type
+	cout 	<< s16 	<< endl;
+	cout 	<< s32 	<< endl;
+	cout 	<< s64 	<< endl;
+	cout 	<< endl;
+
+	//Test safeAdd_min function (custom min/default max, no overage tracking)
+	s8	= safeAdd_min<uint8_t>( n8, d8, mn8 );
+	s16 = safeAdd_min<uint16_t>( n16, d16, mn16 );
+	s32 = safeAdd_min<uint32_t>( n32, d32, mn32 );
+	s64 = safeAdd_min<uint64_t>( n64, d64, mn64 );
+	
+	cout	<< s8  	<< endl; //Outputs as char... because uint8_t doesn't really exist as an unsigned integer type
+	cout 	<< s16 	<< endl;
+	cout 	<< s32 	<< endl;
+	cout 	<< s64 	<< endl;
+	cout 	<< endl;
+
+	//Test safeAdd_max function (default min/cutom max, no overage tracking)
+	s8	= safeAdd_max<uint8_t>( n8, d8, mx8 );
+	s16 = safeAdd_max<uint16_t>( n16, d16, mx16 );
+	s32 = safeAdd_max<uint32_t>( n32, d32, mx32 );
+	s64 = safeAdd_max<uint64_t>( n64, d64, mx64 );
+	
+	cout	<< s8  	<< endl; //Outputs as char... because uint8_t doesn't really exist as an unsigned integer type
+	cout 	<< s16 	<< endl;
+	cout 	<< s32 	<< endl;
+	cout 	<< s64 	<< endl;
+	cout 	<< endl;
 	
 	//Test safeAdd_over function (default min/max, returns overage)
-	s8 = safeAdd_over<uint8_t>( n8, d8, o8 );
-	cout << s8 << "with overage of:" << o8 << endl;
-	
+	s8	= safeAdd_over<uint8_t>( n8, d8, o8 );
 	s16 = safeAdd_over<uint16_t>( n16, d16, o16 );
-	cout << s16 << "with overage of:" << o16 << endl;
-	
 	s32 = safeAdd_over<uint32_t>( n32, d32, o32 );
-	cout << s32 << "with overage of:" << o32 << endl;
-	
 	s64 = safeAdd_over<uint64_t>( n64, d64, o64 );
-	cout << s64 << "with overage of:" << o64 << endl;
+	
+	cout	<< s8	<< " with overage of: "	<< o8 	<< endl;
+	cout 	<< s16 	<< " with overage of: " << o16 	<< endl;
+	cout 	<< s32 	<< " with overage of: " << o32 	<< endl;
+	cout 	<< s64 	<< " with overage of: " << o64 	<< endl;
+	cout	<< endl;
+	
+	//Test safeAdd function (custom min/max, returns overage)
+	s8	= safeAdd<uint8_t>( n8, d8, o8, mn8, mx8 );
+	s16 = safeAdd<uint16_t>( n16, d16, o16, mn16, mx16 );
+	s32 = safeAdd<uint32_t>( n32, d32, o32, mn32, mx32 );
+	s64 = safeAdd<uint64_t>( n64, d64, o64, mn64, mx64 );
+	
+	cout	<< s8	<< " with overage of: "	<< o8 	<< endl;
+	cout 	<< s16 	<< " with overage of: " << o16 	<< endl;
+	cout 	<< s32 	<< " with overage of: " << o32 	<< endl;
+	cout 	<< s64 	<< " with overage of: " << o64 	<< endl;
+	cout	<< endl;
 	
 	//Test asDouble method
-	cout << f8.asDouble() << endl;
-	cout << f16.asDouble() << endl;
-	cout << f32.asDouble() << endl;
-	cout << f64.asDouble() << endl;
+	cout	<< f8.asDouble()	<< endl;
+	cout 	<< f16.asDouble() 	<< endl;
+	cout 	<< f32.asDouble() 	<< endl;
+	cout 	<< f64.asDouble() 	<< endl;
 	
 	//Test asString method
 	/*
--- safemath.hpp
+++ safemath.hpp
@@ -7,21 +7,27 @@ using namespace std;
 
 template <typename TN> class SafeParameters
 {
+	private:
+		TN					overage_count	= 0;
+		bool				exceeded_max	= false;
+		bool				allow_rollover	= false;
+	
 	public:
 		//ABSMIN and ABSMAX are static values, always the same for a given TypeName, TN
 		//Using standard libary numeric_limits for the type
-		static const TN		ABSMIN	= numeric_limits<TN>::min();
-		static const TN		ABSMAX 	= numeric_limits<TN>::max();
+		static const TN			ABSMIN		= numeric_limits<TN>::min();
+		static const TN			ABSMAX 		= numeric_limits<TN>::max();
+		static const uint16_t	precision	= sizeof(TN);
 		
 		//Defaults set here are so even new SafeParameters statment has values assigned
-		TN 					min		= ABSMIN; 		//Minimum Safe Value
-		TN 					max		= ABSMAX; 		//Maximum Safe Value
+		TN 						min		= ABSMIN; 		//Minimum Safe Value
+		TN 						max		= ABSMAX; 		//Maximum Safe Value
 		
 		//This is a placeholder intended to be overwritten with the overage result of the operation
 		//Initialized to 0 - assume no overage unless one is calculated
-		TN 					overage = 0;
+		TN 						overage = 0;
 		
-		SafeParameters( TN thismin = ABSMIN, TN thismax = ABSMAX )
+		SafeParameters( TN thismin = ABSMIN, TN thismax = ABSMAX, bool rollover = false )
 		{
 			//Contructor: if parameters not passed, default min is ABSMIN, default max is ABSMAX
 			//Initial check insures that max > min
@@ -36,9 +42,12 @@ template <typename TN> class SafeParameters
 				max = thismin;
 			}
 			
-			//Set an initial value
+			//Set rollover mode
+			allow_rollover = rollover;
 			
+			//Set an initial value for overage
 			overage = 0;
+			
 		}
 		
 		bool isValid()
@@ -52,9 +61,90 @@ template <typename TN> class SafeParameters
 		bool checkValue( TN value )
 		{
 			//Checks if passed value is valid given the parameters
-			bool valid = ( isValid && ( value >= min ) && ( value <= max ) );
+			bool valid = ( isValid && ( value >= min ) && ( value <= max ) && !exceeded_max );
 			
 			return valid;
+		}
+		
+		TN	addOverage( TN thisoverage )
+		{
+			if ( allow_rollover && ( thisoverage != 0 ) ) //Shortcut entire function if no overage exists or rollover not allowed
+			{
+				SafeParameters<TN> tempparams;
+				
+				//Always use the default MIN/MAX - the overall limit of the variable may be limited by data parameters
+				//But the overages can and should be limited only to the limit of the data type
+				//This ensures we don't quickly run past overage limit if the data limits are very strict
+				overage = safeAdd_base( overage, thisoverage, tempparams );
+				
+				//Possible Scenarios:
+				//Positive Overage exists or is 0, and additional positive overage is less than margin ("Normal")
+				//Positive Overage exists, and additional positive overage is greater than margin
+				//Positive Overage exists, but new overage is negative (IS this possible?)
+				//Negative Overage exists or is 0, and additional negative overage is greater than margin ("Normal")
+				//Negative Overage exists, and additional overage is less than margin
+				//Negative Overage exists, but new overage is positive (IS this possible?)
+				
+				if ( tempparams.overage > 0 )
+				{
+					//Positive overage scenario ( > max )
+					//What if max is negative?
+					if ( ( overage >= 0 ) && ( max >= 0 ) )
+					{
+						if ( overage_count < tempparams.max )
+						{
+							//Adding another count in positive direction
+							overage_count++;
+							//Overage is now equal to max, which is accounted for in count
+							//So switch to remainder > max * count
+							overage = tempparams.overage;
+						}
+						else
+						{
+							//Now we've exceeded the limits of this function and must flag it
+							exceeded_max = true;
+							//overage will already be set to ABS_MAX
+						}
+					}
+					
+				}
+				else if ( ( tempparams.overage < 0 ) && ( min <= 0 ) )
+				{
+					//Negative overage scenario ( < min )
+					//What if min is positive?
+					if ( overage <= 0 )
+					{
+						
+						if ( overage_count > tempparams.min )
+						{
+							//"Adding" another count in the negative direction
+							overage_count--;
+							//Overage is now equal to min, which is accounted for in count
+							//So switch to remainder < min * count
+							overage = tempparams.overage;
+						}
+						
+					}
+					
+				}
+				
+			}
+			else if ( !allow_rollover )
+			{
+				//If there is no rollover, than the overage just equals the new overage
+				overage = thisoverage;
+			}
+			
+			return overage;
+			
+		}
+		
+		void reset()
+		{
+			//Resets parameters as necessary
+			overage 		= 0;
+			overage_count 	= 0;
+			exceeded_max 	= false;
 		}
 		
 };
